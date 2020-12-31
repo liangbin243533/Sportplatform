21fb1996b2e2b76f33365ae6e4b48ca9fbf3c279
LiangbinChen <chenliangbin123456@126.com>
2020-12-31 21:14:00 +0800

    Adding login and home page back-end logic

--- src/main/java/com/chen/sportplatform/SportplatformApplication.java
+++ src/main/java/com/chen/sportplatform/SportplatformApplication.java
@@ -1,8 +1,10 @@
 package com.chen.sportplatform;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan("com.chen.sportplatform.dao")
 @SpringBootApplication
 public class SportplatformApplication {
 
